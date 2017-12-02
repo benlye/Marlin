dd243aaf4912083a56086a59341498a327c4648b
Ben Lye <ben@lye.co.nz>
2017-12-02 20:23:07 +0000

    Increase grid points to 7 and move back bed position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -923,14 +923,14 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
   #define LEFT_PROBE_BED_POSITION 15
   #define RIGHT_PROBE_BED_POSITION 195
   #define FRONT_PROBE_BED_POSITION 15
-  #define BACK_PROBE_BED_POSITION 180
+  #define BACK_PROBE_BED_POSITION 183
 
   // The Z probe minimum outer margin (to validate G29 parameters).
   #define MIN_PROBE_EDGE 10
